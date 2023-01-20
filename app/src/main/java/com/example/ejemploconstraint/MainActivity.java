8e540bd9e3b1c8984e4f9f1a56c4248971a6f9ff
lorencd23 <lorencd23@gmail.com>
2023-01-20 17:57:52 +0100

    Ej 29

--- app/src/main/java/com/example/ejemploconstraint/MainActivity.java
+++ app/src/main/java/com/example/ejemploconstraint/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ejemploconstraint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // This o getApplicationContext() es lo mismo, pero con la segunda es mejor
+        // por si usamos eventos fuera de esta clase, ya que con this se usan claves anónimas
+        Toast.makeText(getApplicationContext(), "Hola, esto es un Toast", Toast.LENGTH_LONG).show();
+
     }
 }
