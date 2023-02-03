1aacbb24be73cd950bfbb402631f823a01120f86
lorencd23 <lorencd23@gmail.com>
2023-02-03 12:48:14 +0100

    fin

---
+++ skill4/skill4parte11/app/src/main/java/com/example/skill4parte11/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.skill4parte11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listaDias;
+    TextView etiSeleccion;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaDias = (ListView) findViewById(R.id.listViewId);
+        etiSeleccion = (TextView) findViewById(R.id.txtId);
+
+        ArrayList<String> listaNombres = new ArrayList<>();
+        listaNombres.add("Cristian");
+        listaNombres.add("Paco");
+        listaNombres.add("Maria");
+        listaNombres.add("Lucia");
+
+        for(int i = 0; i<10; i++){
+            listaNombres.add("Nombre " + i);
+        }
+
+        ArrayAdapter adaptador = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaNombres);
+
+        listaDias.setAdapter(adaptador);
+
+        listaDias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(), "Selecciona: " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+
+                etiSeleccion.setText("Seleccion: " + parent.getItemAtPosition(position).toString());
+            }
+        });
+    }
+}
