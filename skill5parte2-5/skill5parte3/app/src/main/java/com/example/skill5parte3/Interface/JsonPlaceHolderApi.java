47ca3d38a1108efa38b43a1158c5285ee9800c8b
lorencd23 <lorencd23@gmail.com>
2023-02-09 12:07:16 +0100

    skill5parte3 (sencillo retrofit)

--- skill5parte2-5/skill5parte3/app/src/main/java/com/example/skill5parte3/Interface/JsonPlaceHolderApi.java
+++ skill5parte2-5/skill5parte3/app/src/main/java/com/example/skill5parte3/Interface/JsonPlaceHolderApi.java
@@ -7,8 +7,9 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-public interface JsonPlaceHolderApis {
+public interface JsonPlaceHolderApi {
 
     @GET("posts")
     Call<List<Posts>> getPosts();
+
 }
--- skill5parte2-5/skill5parte3/app/src/main/java/com/example/skill5parte3/MainActivity.java
+++ skill5parte2-5/skill5parte3/app/src/main/java/com/example/skill5parte3/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.skill5parte3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.skill5parte3.Interface.JsonPlaceHolderApi;
+import com.example.skill5parte3.Model.Posts;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mJsonTxtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mJsonTxtView = findViewById(R.id.jsonText);
+        getPosts();
     }
+
+    private void getPosts(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        Call<List<Posts>> call = jsonPlaceHolderApi.getPosts();
+
+        call.enqueue(new Callback<List<Posts>>() {
+            @Override
+            public void onResponse(Call<List<Posts>> call, Response<List<Posts>> response) {
+
+                if(!response.isSuccessful()){
+                    mJsonTxtView.setText("Codigo: " + response.code());
+                    return;
+                }
+
+                List<Posts> postsList = response.body();
+
+                for (Posts post: postsList) {
+                    String content = "";
+                    content += "userId: " + post.getUserId() + "\n";
+                    content += "Id: " + post.getId() + "\n";
+                    content += "title: " + post.getTitle() + "\n";
+                    content += "body: " + post.getBody() + "\n\n";
+
+                    mJsonTxtView.append(content);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Posts>> call, Throwable t) {
+
+                mJsonTxtView.setText(t.getMessage());
+
+            }
+        });
+
+    }
+
 }
